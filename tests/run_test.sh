ae81ce69096250eaf39e16d4caf586830a8a5d3c
Bin.Li <bin.li@noaa.gov>
2020-05-21 12:56:59 +0000

    Updated parm/model_configure.IN, log directory, and tests directory.

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -52,7 +52,7 @@ echo "Test ${TEST_NR} ${TEST_NAME} ${TEST_DESCR}"
 trap 'echo "run_test.sh: Test ${TEST_NAME} killed"; kill $(jobs -p); wait; trap 0; exit' 1 2 3 4 5 6 7 8 10 12 13 15
 trap '[ "$?" -eq 0 ] || write_fail_test' EXIT
 
-RUN_SCRIPT=rt_fv3.sh
+RUN_SCRIPT=rt_datm.sh
 ./${RUN_SCRIPT} > ${RUNDIR_ROOT}/${TEST_NAME}${RT_SUFFIX}.log 2>&1
 
 elapsed=$SECONDS
