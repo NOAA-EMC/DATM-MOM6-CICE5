cc70f458418ec2b21a234a53a7bada17d6f6ac8c
Bin Li <bin.li@noaa.gov>
2020-09-12 14:25:10 -0500

    Added log files from Orion.

--- tests/rt_1degree.sh
+++ tests/rt_1degree.sh
@@ -229,7 +229,7 @@ elif [[ $MACHINE_ID = orion.* ]]; then
 #  ACCNR= # detected in detect_machine.sh
   PARTITION=orion
   dprefix=/work/noaa/stmp/${USER}
-  DISKNM=/work/noaa/stmp/libin/RT
+  DISKNM=/work/noaa/nems/emc.nemspara/RT
   STMP=$dprefix/test
   PTMP=$dprefix/test
 
